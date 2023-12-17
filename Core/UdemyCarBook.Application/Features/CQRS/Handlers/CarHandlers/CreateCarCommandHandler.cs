77f644f1f8beca48481636c113bd628b608f8a80
korog <korog@LAPTOP-GPMKKC5N>
2023-12-17 14:42:51 +0300

    car alanı yapıldı.

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/CarHandlers/CreateCarCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Commands.CarCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers
+{
+	public class CreateCarCommandHandler
+	{
+		private readonly IRepository<Car> _repository;
+		public CreateCarCommandHandler(IRepository<Car> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateCarCommand command)
+		{
+			await _repository.CreateAsync(new Car
+			{
+				Km = command.Km,
+				Luggage = command.Luggage,
+				Model = command.Model,
+				Seat = command.Seat,
+				Transmission = command.Transmission,
+				Fuel = command.Fuel,
+				CoverImageUrl = command.CoverImageUrl,
+				BrandID = command.BrandID,
+				BigImageUrl = command.BigImageUrl
+			});
+		}
+	}
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/CarHandlers/GetCarByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Queries.CarQueries;
+using UdemyCarBook.Application.Features.CQRS.Results.CarResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers
+{
+	public class GetCarByIdQueryHandler
+	{
+		private readonly IRepository<Car> _repository;
+		public GetCarByIdQueryHandler(IRepository<Car> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<GetCarByIdQueryResult> Handle(GetCarByIdQuery query)
+		{
+			var values = await _repository.GetByIdAsync(query.Id);
+			return new GetCarByIdQueryResult
+			{
+				CarID = values.CarID,
+				BigImageUrl = values.BigImageUrl,
+				BrandID = values.BrandID,
+				CoverImageUrl = values.CoverImageUrl,
+				Fuel = values.Fuel,
+				Transmission = values.Transmission,
+				Seat = values.Seat,
+				Model = values.Model,
+				Luggage = values.Luggage,
+				Km = values.Km
+			};
+		}
+	}
+}
