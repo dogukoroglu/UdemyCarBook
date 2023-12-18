70ea8964e682ad4a3b79d24f514c0194f7b78229
korog <korog@LAPTOP-GPMKKC5N>
2023-12-19 01:53:47 +0300

    Testimonial consume işlemleri tamamlandı.

--- Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationByIdQueryHandler.cs
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationByIdQueryHandler.cs
@@ -12,10 +12,10 @@ using UdemyCarBook.Domain.Entities;
 
 namespace UdemyCarBook.Application.Features.Mediator.Handlers.LocationHandlers
 {
-	public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResult>
+	public class GetTestimonialByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResult>
 	{
 		private readonly IRepository<Location> _repository;
-		public GetLocationByIdQueryHandler(IRepository<Location> repository)
+		public GetTestimonialByIdQueryHandler(IRepository<Location> repository)
 		{
 			_repository = repository;
 		}
--- Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationQueryHandler.cs
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationQueryHandler.cs
@@ -12,10 +12,10 @@ using UdemyCarBook.Domain.Entities;
 
 namespace UdemyCarBook.Application.Features.Mediator.Handlers.LocationHandlers
 {
-	public class GetLocationQueryHandler : IRequestHandler<GetLocationQuery, List<GetLocationQueryResult>>
+	public class GetTestimonialQueryHandler : IRequestHandler<GetLocationQuery, List<GetLocationQueryResult>>
 	{
 		private readonly IRepository<Location> _repository;
-		public GetLocationQueryHandler(IRepository<Location> repository)
+		public GetTestimonialQueryHandler(IRepository<Location> repository)
 		{
 			_repository = repository;
 		}
