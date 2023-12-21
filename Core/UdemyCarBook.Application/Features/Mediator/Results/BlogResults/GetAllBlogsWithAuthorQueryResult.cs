56e7133c4fd59af97f77cae6c637fa0d6b3994a8
korog <korog@LAPTOP-GPMKKC5N>
2023-12-22 02:24:53 +0300

    Blog düzenlemeleri yapılıyor.

--- Core/UdemyCarBook.Application/Features/Mediator/Results/BlogResults/GetAllBlogsWithAuthorQueryResult.cs
+++ Core/UdemyCarBook.Application/Features/Mediator/Results/BlogResults/GetAllBlogsWithAuthorQueryResult.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UdemyCarBook.Domain.Entities;
 
 namespace UdemyCarBook.Application.Features.Mediator.Results.BlogResults
 {
-	public class GetBlogByIdQueryResult
+	public class GetAllBlogsWithAuthorQueryResult
 	{
 		public int BlogID { get; set; }
 		public string Title { get; set; }
+		public string AuthorName { get; set; }
+		public string CategoryName { get; set; }
 		public int AuthorID { get; set; }
 		public string CoverImageUrl { get; set; }
 		public DateTime CreateDate { get; set; }
 		public int CategoryID { get; set; }
-
+		public string Description { get; set; }
 	}
 }
--- Infrastructure/UdemyCarBook.Persistence/Repositories/BlogRepositories/BlogRepository.cs
+++ Infrastructure/UdemyCarBook.Persistence/Repositories/BlogRepositories/BlogRepository.cs
@@ -18,6 +18,12 @@ namespace UdemyCarBook.Persistence.Repositories.BlogRepositories
 			_carBookContext = carBookContext;
 		}
 
+		public List<Blog> GetAllBlogsWithAuthors()
+		{
+			var values = _carBookContext.Blogs.Include(x => x.Author).ToList();
+			return values;
+		}
+
 		public List<Blog> GetLast3BlogsWithAuthors()
 		{
 			var values = _carBookContext.Blogs.Include(x => x.Author).OrderByDescending(y=>y.BlogID).Take(3).ToList();
