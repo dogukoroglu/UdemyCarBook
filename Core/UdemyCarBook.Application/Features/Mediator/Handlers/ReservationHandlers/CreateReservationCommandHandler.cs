f013d21738734d7fa482214aa1b9632bc1c183b0
korog <korog@LAPTOP-GPMKKC5N>
2024-01-20 02:56:42 +0300

    Reservation tablosu için api yazılıyor.

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/ReservationHandlers/CreateReservationCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commands.ReservationCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.ReservationHandlers
+{
+	public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand>
+	{
+		private readonly IRepository<Reservation> _repository;
+		public CreateReservationCommandHandler(IRepository<Reservation> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.CreateAsync(new Reservation
+			{
+				Name = request.Name,
+				Surname = request.Surname,
+				Age = request.Age,
+				Description = request.Description,
+				CarID = request.CarID,
+				DriverLicenseYear = request.DriverLicenseYear,
+				DropOffLocationID = request.DropOffLocationID,
+				Email = request.Email,
+				PhoneNumber = request.PhoneNumber,
+				PickUpLocationID = request.PickUpLocationID
+			});
+		}
+	}
+}
---
+++ Presentation/UdemyCarBook.WebApi/Controllers/ReservationsController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarBook.Application.Features.Mediator.Commands.ReservationCommands;
+namespace UdemyCarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ReservationsController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+		public ReservationsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		//[HttpGet]
+		//public async Task<IActionResult> ReservationList()
+		//{
+		//	var values = await _mediator.Send(new GetReservationQuery());
+		//	return Ok(values);
+		//}
+
+		//[HttpGet("{id}")]
+		//public async Task<IActionResult> GetReservation(int id)
+		//{
+		//	var value = await _mediator.Send(new GetReservationByIdQuery(id));
+		//	return Ok(value);
+		//}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateReservation(CreateReservationCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Rezervasyon başarıyla eklendi!");
+		}
+
+		//[HttpDelete("{id}")]
+		//public async Task<IActionResult> RemoveReservation(int id)
+		//{
+		//	await _mediator.Send(new RemoveReservationCommand(id));
+		//	return Ok("Referans başarıyla silindi!");
+		//}
+
+		//[HttpPut]
+		//public async Task<IActionResult> UpdateReservation(UpdateReservationCommand command)
+		//{
+		//	await _mediator.Send(command);
+		//	return Ok("Referans başarıyla güncellendi!");
+		//}
+	}
+}
