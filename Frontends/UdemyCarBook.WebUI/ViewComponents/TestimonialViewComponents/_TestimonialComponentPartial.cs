014f0a80347f61a8e1dbd3e935d1704f3d35a2dd
korog <korog@LAPTOP-GPMKKC5N>
2024-02-07 02:34:34 +0300

    footer alanı düzenlendi.

--- Frontends/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -4,25 +4,25 @@ using UdemyCarBook.Dto.TestimonialDtos;
 
 namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponents
 {
-    public class _TestimonialComponentPartial : ViewComponent
-    {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+	public class _TestimonialComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7022/api/Testimonials");
-            if(responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
-                return View(values);
-            }
-            return View();
-        }
-    }
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7022/api/Testimonials");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
