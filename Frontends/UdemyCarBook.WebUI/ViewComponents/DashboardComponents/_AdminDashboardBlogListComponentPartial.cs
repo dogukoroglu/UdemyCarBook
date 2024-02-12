966a7fdd5be90b35eebb15c724827bc7364c6ea5
korog <korog@LAPTOP-GPMKKC5N>
2024-02-13 00:12:52 +0300

    dashboard tamamlandı.

---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.Dto.BlogDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DashboardComponents
+{
+	public class _AdminDashboardBlogListComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7022/api/Blogs/GetAllBlogsWithAuthorList");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultAllBLogsWithAuthorDto>>(jsonData);
+				return View(values.Take(4).ToList());
+			}
+			return View();
+		}
+	}
+}
---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardCarPricingListComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.Dto.CarPricingDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DashboardComponents
+{
+	public class _AdminDashboardCarPricingListComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public _AdminDashboardCarPricingListComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			ViewBag.v1 = "Paketler";
+			ViewBag.v2 = "Araç Fiyat Paketleri";
+
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7022/api/CarPricings/GetCarPricingWithTimePeriodList");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+				return View(values.Take(5).ToList());
+			}
+			return View();
+		}
+	}
+}
